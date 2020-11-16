f6568679c27d348aca7f412874aaa193426f7dd2
juanarton <juanarton@protonmail.com>
2020-11-16 20:43:13 +0700

    Praktikun6

---
+++ Praktikum6/src/main/java/com/JuanArtonM_18102199/Praktikum6/MyData.kt
@@ -0,0 +1,13 @@
+package com.JuanArtonM_18102199.Praktikum6
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class MyData(
+    var name: String,
+    var description: String,
+    var photo: String
+) : Parcelable
+
+
