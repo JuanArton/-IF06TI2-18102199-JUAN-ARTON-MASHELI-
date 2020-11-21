23141d5f7e25fac522997c006bec625e41ea1776
juanarton <juanarton@protonmail.com>
2020-11-21 08:28:44 +0700

    Praktikum 7

--- Praktikum6/src/main/java/com/JuanArtonM_18102199/Praktikum6/adapter/CardViewMyDataAdapter.kt
+++ Praktikum6/src/main/java/com/JuanArtonM_18102199/Praktikum6/adapter/CardViewMyDataAdapter.kt
@@ -1,5 +1,7 @@
 package com.JuanArtonM_18102199.Praktikum6.adapter
 
+import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,12 +10,13 @@ import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import com.JuanArtonM_18102199.Praktikum6.DetailActivity
 import com.JuanArtonM_18102199.Praktikum6.MyData
 import com.JuanArtonM_18102199.Praktikum6.R
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 
-class CardViewMyDataAdapter(private val listMyDatas: ArrayList<MyData>) :
+class CardViewMyDataAdapter(private val listMyDatas: ArrayList<MyData>, val context: Context) :
     RecyclerView.Adapter<CardViewMyDataAdapter.CardViewViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardViewViewHolder {
         val view: View = LayoutInflater.from(parent.context).inflate(R.layout.item_cardview, parent, false)
@@ -33,7 +36,15 @@ class CardViewMyDataAdapter(private val listMyDatas: ArrayList<MyData>) :
                 listMyDatas[holder.adapterPosition].name, Toast.LENGTH_SHORT).show() }
         holder.itemView.setOnClickListener { Toast.makeText(holder.itemView.context, "Kamu memilih " +
                 listMyDatas[holder.adapterPosition].name, Toast.LENGTH_SHORT).show() }
+
+        holder.itemView.setOnClickListener {
+            val moveWithObjectIntent = Intent(context, DetailActivity::class.java)
+            moveWithObjectIntent.putExtra(DetailActivity.EXTRA_MYDATA, myData)
+            context.startActivity(moveWithObjectIntent)
+        }
     }
+
+
     override fun getItemCount(): Int {
         return listMyDatas.size
     }
--- Praktikum6/src/main/java/com/JuanArtonM_18102199/Praktikum6/adapter/ListMyDataAdapter.kt
+++ Praktikum6/src/main/java/com/JuanArtonM_18102199/Praktikum6/adapter/ListMyDataAdapter.kt
@@ -1,23 +1,34 @@
 package com.JuanArtonM_18102199.Praktikum6.adapter
 
+import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.JuanArtonM_18102199.Praktikum6.DetailActivity
 import com.JuanArtonM_18102199.Praktikum6.MyData
 import com.JuanArtonM_18102199.Praktikum6.R
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import kotlinx.android.synthetic.main.item_list.view.*
 
-class ListMyDataAdapter(private val listMyData: ArrayList<MyData>) :
+class ListMyDataAdapter(private val listMyData: ArrayList<MyData>, val context: Context) :
     RecyclerView.Adapter<ListMyDataAdapter.ListViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_list, parent, false)
         return ListViewHolder(view)
     }
     override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+        val myData = listMyData[position]
+
         holder.bind(listMyData[position])
+
+        holder.itemView.setOnClickListener {
+            val moveWithObjectIntent = Intent(context, DetailActivity::class.java)
+            moveWithObjectIntent.putExtra(DetailActivity.EXTRA_MYDATA, myData)
+            context.startActivity(moveWithObjectIntent)
+        }
     }
     override fun getItemCount(): Int = listMyData.size
     inner class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
